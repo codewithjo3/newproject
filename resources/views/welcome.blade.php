1fd5cb7205092b6b3b2183106f1cfcea27f6e1ab
codewithjo3 <sometimewithjoel@gmail.com>
2021-03-17 11:57:41 +0530

    message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1><center>REGISTRATION</center></h1>
+   <div class="container">
+   <div class="row">
+   <div class="col col-12 col-sm-3 col-lg-3 col-xl-3">
+   <div class="col col-12 col-sm-3 col-lg-3 col-xl-3">
+   </div>
+   </div>
+   </div>
+    <table class="table">
+
+    <tr>
+        <td>name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>roll number</td>
+        <td><input type="number" class="form-control"></td>
+    </tr>
+
+    <tr>
+        <td>Admission number</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+
+    <tr>
+        <td>College name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+
+
+    
+    </table>
+</body>
+</html>
